e088e0762fbef9bc004ec5ff40912ae01b3e07bf
Aarushi <160153768+AarushiTheBug@users.noreply.github.com>
2024-02-27 14:38:19 +0530

    Add files via upload

---
+++ constructord.java
@@ -0,0 +1,12 @@
+class def{
+    int id=23;
+    String name="aarushi";
+    def(){
+        System.out.println("name is"+" " + name+"  id is "+""+id);
+    }
+    public static void main(String[] args) {
+        def s = new def();
+        //student s1=new student();
+        //s.display();
+    }
+}
